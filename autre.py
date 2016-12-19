53e6e1f2dd0b6f133e0d1d971b529daa680ec822
Michael BLOT <blot@poleia.lip6.fr>
2016-12-19 17:50:36 +0100

    tests

--- autre.py
+++ autre.py
@@ -1,8 +1,77 @@
 import tensorflow as tf
-sess = tf.Session()
-x = tf.Variable([1.0, 2.0])
-sess.run(tf.initialize_all_variables())
+import threading as th
+from data.data import Data
+from data.metadata import get_metadata
+from config import init_config
+import numpy as np
+import time
 
-print(x.eval(session=sess))
-x = 2* x
-print(x.eval(session=sess))
+opts = init_config()
+
+#initialization of datasets
+meta_data = get_metadata()
+train_set = Data(meta_data['train'])
+n_thread = 1
+nBatch = 128
+batch_size = 253*256
+
+sess = tf.Session()
+data_q = tf.FIFOQueue(10000 , [tf.float32], shapes=[batch_size, 1])
+compt_q = tf.FIFOQueue(1 , tf.int64)
+
+compteur = tf.placeholder(tf.int64, [])
+data = tf.placeholder(tf.float32, [batch_size, 1])
+
+data_inc = data_q.enqueue(data)
+data_dec = data_q.dequeue()
+compt_inc = compt_q.enqueue(compteur)
+compt_dec = compt_q.dequeue()
+
+sess.run(compt_inc, feed_dict={compteur:0})
+run_options = tf.RunOptions(timeout_in_ms=100)
+
+
+def data_fill(coord, nBatch, dataset, batch_size):
+    pass
+    while not coord.should_stop():
+        try:
+            a = sess.run(compt_dec, options=run_options)
+            a = a + 1
+            if a >= nBatch:
+                coord.request_stop()
+            print(a)
+            inputs = dataset.sample( batch_size )['inputs']
+            sess.run(compt_inc, feed_dict={compteur:a})
+            #sess.run(data_inc, feed_dict={data:inputs})
+        except tf.errors.DeadlineExceededError:
+            print("empty")
+            pass
+
+def data_use(coord):
+    while not coord.should_stop():#
+        a = sess.run(compt_dec, options=run_options)
+        print(a)
+        if a >= nBatch:
+            coord.request_stop()
+        sess.run(compt_inc, feed_dict={compteur:a})
+        u = sess.run(data_dec)
+        #print(np.shape(u))
+
+coord_inc = tf.train.Coordinator()
+#coord_dec = tf.train.Coordinator()
+
+threads_inc = [th.Thread(target=data_fill, args =(coord_inc, nBatch, train_set, batch_size)) for i in range(n_thread)]
+#threads_dec = [th.Thread(target=data_use, args =(coord_dec,)) for i in range(n_thread)]
+
+begin = time.clock()
+
+for t in threads_inc: t.start()
+#for t in threads_dec: t.start()
+
+coord_inc.join(threads_inc)
+#coord_dec.request_stop()
+#coord_dec.join(threads_dec)
+
+end = time.clock()
+
+print(end - begin)
--- try.py
+++ try.py
@@ -1,50 +1,72 @@
 import tensorflow as tf
 import threading as th
+from data.data import Data
+from data.metadata import get_metadata
+from config import init_config
+import numpy as np
+import time
+
+opts = init_config()
+
+#initialization of datasets
+meta_data = get_metadata()
+train_set = Data(meta_data['train'])
+n_thread = 8
+nBatch = 30
+batch_size = 128
+
 sess = tf.Session()
+data_q = tf.FIFOQueue(nBatch , [tf.float32], shapes=[batch_size, 3, 224, 224])
+compt_q = tf.FIFOQueue(1 , tf.int64)
 
-q = tf.FIFOQueue(8 , "float")
-z = tf.placeholder("float", 1)
-q_inc = q.enqueue(z[0])
-q_dec = q.dequeue()
+compteur = tf.placeholder(tf.int64, [])
+data = tf.placeholder(tf.float32, [batch_size, 3, 224, 224])
 
+data_inc = data_q.enqueue(data)
+data_dec = data_q.dequeue()
+compt_inc = compt_q.enqueue(compteur)
+compt_dec = compt_q.dequeue()
+data_size = data_q.size()
 
+sess.run(compt_inc, feed_dict={compteur:0})
+run_options = tf.RunOptions(timeout_in_ms=100)
 
-nBatch = 20
-y = 1.
-
-
-compt = tf.Variable([1.0])
-init = tf.global_variables_initializer()
-sess.run(init)
-print(compt.eval(session=sess))
-
-def inc(coord, y, compt, nBatch):
+def data_fill(coord, nBatch, batch_size):
     while not coord.should_stop():
-        compt = compt + [1.]
-        a = 1#compt.eval(sess)
+        inputs = np.random.rand(batch_size,3,224,224)
+        sess.run(data_inc, feed_dict={data:inputs})
+
+def data_use(coord):
+    while not coord.should_stop():#
+        a = sess.run(compt_dec, options=run_options)
         print(a)
-        sess.run(q_inc, feed_dict={z:[a]})
-        y = y + 1
-        #print(compt.eval(sess))
-        if y == nBatch:
+        if a >= nBatch:
             coord.request_stop()
-
-def dec(coord):
-    while not coord.should_stop():
-        a = sess.run(q_dec)
-        #print('dec %g'%(a))
-
+        sess.run(compt_inc, feed_dict={compteur:a})
+        u = sess.run(data_dec)
+        #print(np.shape(u))
 
 coord_inc = tf.train.Coordinator()
-coord_dec = tf.train.Coordinator()
-n_thread = 4
-threads_inc = [th.Thread(target=inc, args =(coord_inc, y, compt, nBatch)) for i in range(n_thread)]
-threads_dec = [th.Thread(target=dec, args =(coord_dec,)) for i in range(1)]
+#coord_dec = tf.train.Coordinator()
 
+threads_inc = [th.Thread(target=data_fill, args =(coord_inc, nBatch, batch_size)) for i in range(n_thread)]
+#threads_dec = [th.Thread(target=data_use, args =(coord_dec,)) for i in range(n_thread)]
+
+begin = time.clock()
 
 for t in threads_inc: t.start()
-for t in threads_dec: t.start()
+#for t in threads_dec: t.start()
+
+while(sess.run(data_size) < nBatch): pass
+end = time.clock()
+print(end - begin)
+coord_inc.request_stop()
+sess.run(data_q.close(cancel_pending_enqueues=True))
 
 coord_inc.join(threads_inc)
-coord_dec.request_stop()
-coord_dec.join(threads_dec)
+#coord_dec.request_stop()
+#coord_dec.join(threads_dec)
+
+end = time.clock()
+
+#print(end - begin)
